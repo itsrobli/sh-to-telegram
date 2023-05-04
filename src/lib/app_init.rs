205f8ae2394987665d485b6ed6cac84747038be6
Robert Li <robertwli@gmail.com>
2023-05-05 00:08:56 +1000

    app state logic

--- src/lib/app_init.rs
+++ src/lib/app_init.rs
@@ -1,7 +1,27 @@
-use crate::config::ConfigFileState;
+use std::path::PathBuf;
+use crate::config::{Config, ConfigError, ConfigFileState};
 use crate::logger::LogFileState;
 
 pub struct App {
     config_file_state: ConfigFileState,
     log_file_state: LogFileState,
+}
+
+impl App {
+    fn new() -> Self {
+        let mut config_file_state = ConfigFileState;
+        let mut log_file_state = LogFileState;
+        match config_file_state = Config::default_path() {
+            Ok(path) => {
+                if path.exists() {
+                    ConfigFileState::Exists
+                }
+            }
+            Err(_) => ConfigFileState::NotExists
+        }
+        Self {
+            config_file_state,
+            log_file_state,
+        }
+    }
 }
