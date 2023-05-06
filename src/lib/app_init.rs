42782f9f19610bd4cf9c467df1ea490033acc12d
Robert Li <robertwli@gmail.com>
2023-05-06 17:48:25 +1000

    app state logic - handle config only error

--- src/lib/app_init.rs
+++ src/lib/app_init.rs
@@ -9,7 +9,7 @@ pub struct App {
 }
 
 impl App {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let mut config_file_state = ConfigFileState::NotExists;
         let mut log_file_state = LogFileState::NotExists;
         config_file_state = match Config::default_path() {
@@ -35,18 +35,37 @@ impl App {
             log_file_state,
         }
     }
-    fn init(&mut self) -> Result<(), AppError> {
+    pub fn init(&mut self) -> Result<(), AppError> {
         match (&self.config_file_state, &self.log_file_state) {
             (ConfigFileState::Exists, LogFileState::Exists) => Ok(()),
             (ConfigFileState::Exists, LogFileState::NotExists) => {
                 todo!()
             },
             (ConfigFileState::NotExists, LogFileState::Exists) => {
-                todo!()
+                match App::handle_no_config_file() {
+                    Ok(_) => Err(AppError::NeedsOffAndOn),
+                    Err(err) => Err(AppError::Config(err))
+                }
             },
             (ConfigFileState::NotExists, LogFileState::NotExists) => {
                 todo!()
             },
+        }
+    }
+    fn handle_no_config_file() -> Result<(), ConfigError> {
+        match Config::create_template_config_file() {
+            Ok(_) => {
+                println!(
+                    "New user. Please setup configs in you user dir {}",
+                    Config::print_default_config_path()
+                );
+                println!("Then run this program again. \n Goodbye!");
+                Ok(())
+            }
+            Err(err) => {
+                println!("{}", err);
+                Err(err)
+            }
         }
     }
 }
@@ -55,4 +74,8 @@ impl App {
 pub enum AppError {
     #[error("app needs to shutdown and be re-run")]
     NeedsOffAndOn,
+    #[error("config error when initiating app")]
+    Config(ConfigError),
+    #[error("app unknown error")]
+    Unknown,
 }
